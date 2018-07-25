af451077e32784c82ec9d9511ecdbb6a570ef2ce
Nismo-Gtr <NCowan3@outlook.com>
2018-07-24 22:30:43 -0500

    Added brand statement

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Row, Col, Card, CardTitle } from 'react-materialize';
 import homeOffice from '../../assets/home-office.jpg';
+import trivia from '../../assets/trivia-time.png'
 import friendFinder from '../../assets/Friend-Finder.png';
 import burger from '../../assets/Build-a-burger.png';
 import clickGame from '../../assets/click-game.png'
@@ -22,25 +23,25 @@ class Portfolio extends Component {
                 <h2 className="header center">Portfolio</h2>
                 <Row>
                     <div data-aos="fade-right" data-aos-delay={100}>
-                        <Col m={4}>
-                            <Card header={<CardTitle reveal image={homeOffice} waves='light' />}
-                                title="Mongo News Scraper"
-                                reveal={<p>Here is some more information about this product that is only revealed once clicked on.<br />
-                                    <a href="https://friendfindernjc.herokuapp.com/">Friend Finder App</a></p>}>
+                        <Col m={4} s={12}>
+                            <Card header={<CardTitle reveal image={trivia} waves='light' />}
+                                title="Trivia Time!"
+                                reveal={<p>A trivia game that pulls the question on the fly from the OpenTrivia api. Built using react, react-router, OpenTrivia api, axios, and react-materialize<br />
+                                    <a href="https://nismo-gtr.github.io/TriviaGame/">Play Trivia Time!</a></p>}>
                             </Card>
                         </Col>
                     </div>
                     <div data-aos="fade-up" data-aos-delay={100}>
-                        <Col m={4}>
+                        <Col m={4} s={12}>
                             <Card header={<CardTitle reveal image={clickGame} waves='light' />}
                                 title="React Click Game"
                                 reveal={<p>Here is some more information about this product that is only revealed once clicked on.<br />
-                                    <a href="https://friendfindernjc.herokuapp.com/">World Cup of Clicks</a></p>}>
+                                    <a href="https://nismo-gtr.github.io/friend-card-memory/">World Cup of Clicks</a></p>}>
                             </Card>
                         </Col>
                     </div>
                     <div data-aos="fade-left" data-aos-delay={100}>
-                        <Col m={4}>
+                        <Col m={4} s={12}>
                             <Card header={<CardTitle reveal image={burger} waves='light' />}
                                 title="Build a Burger App"
                                 reveal={<p>Here is some more information about this product that is only revealed once clicked on.<br />
@@ -51,7 +52,7 @@ class Portfolio extends Component {
                 </Row>
                 <Row>
                     <div data-aos="fade-right" data-aos-delay={600}>
-                        <Col m={4}>
+                        <Col m={4} s={12}>
                             <Card header={<CardTitle reveal image={friendFinder} waves='light' />}
                                 title="Friend Finder"
                                 reveal={<p>Here is some more information about this product that is only revealed once clicked on.<br />
@@ -60,7 +61,7 @@ class Portfolio extends Component {
                         </Col>
                     </div>
                     <div data-aos="fade-up" data-aos-delay={600}>
-                        <Col m={4}>
+                        <Col m={4} s={12}>
                             <Card header={<CardTitle reveal image={homeOffice} waves='light' />}
                                 title="Gettaway App"
                                 reveal={<p>Here is some more information about this product that is only revealed once clicked on.<br />
@@ -69,7 +70,7 @@ class Portfolio extends Component {
                         </Col>
                     </div>
                     <div data-aos="fade-left" data-aos-delay={600}>
-                        <Col m={4}>
+                        <Col m={4} s={12}>
                             <Card header={<CardTitle reveal image={homeOffice} waves='light' />}
                                 title="Card Title"
                                 reveal={<p>Here is some more information about this product that is only revealed once clicked on.<br />
--- src/components/aboutMe/aboutMe.js
+++ src/components/aboutMe/aboutMe.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Row, Col, Card } from 'react-materialize';
+import { Row, Col } from 'react-materialize';
 import AOS from 'aos';
 import sequelLogo from '../../assets/sequelize-original-wordmark.svg';
 import mysql from '../../assets/mysql-original-wordmark.svg';
@@ -35,60 +35,63 @@ class AboutMe extends Component {
                 </Row>
                 <Row>
                     <Col s={12}>
-                        <p id="brandStatement">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <p id="brandStatement">Ambitious Full Stack Developer skilled in creating immersive user experiences on the internet that look great on any device whether it be a desktop, notebook, tablet or phone. Experienced and up to date on the latest developments and trends in HTML, CSS, and JavaScript both vanilla and many frameworks. 
+
+I am also pursuing my A.S. in Software Development with the intention of transferring and obtaining a Bachelor’s Degree in Computer Science. Technical courses completed include Micro-Computer Database Applications, HTML/CSS, Intro to Networking, Intro to Linux/Unix OS, Intro Computer Information Systems, and Windows Command Line.
+</p>
                     </Col>
                 </Row>
                 <Row>
-                    <Col m={3} s={12} data-aos="flip-right">
-                        <Card className='white darken-1 skills skills' textClassName='black-text' actions={[<a href='https://mongodb.com'>MongoDB Homepage</a>]}>
+                    <Col m={3} s={12} data-aos="fade-up">
+                        <div className='white darken-1 skills skills' textClassName='black-text' actions={[<a href='https://mongodb.com'>MongoDB Homepage</a>]}>
                             <img id="mongo" src={mongo} alt="mongoDB logo" />
                             <p>MongoDB is a free and open-source cross-platform document-oriented database program. Classified as a NoSQL database program, MongoDB uses JSON-like documents with schemas. MongoDB is developed by MongoDB Inc., and is published under a combination of the GNU Affero General Public License and the Apache License.</p>
-                        </Card>
+                        </div>
                     </Col>
-                    <Col m={3} s={12} data-aos="flip-right">
-                        <Card className='white darken-1 skills' textClassName='black-text' actions={[<a href='https://expressjs.com'>Express Homepage</a>]}>
+                    <Col m={3} s={12} data-aos="fade-up">
+                        <div className='white darken-1 skills' textClassName='black-text' actions={[<a href='https://expressjs.com'>Express Homepage</a>]}>
                             <img id="express" src={express} alt="express logo" />
                             <p>Express.js, or simply Express, is a web application framework for Node.js, released as free and open-source software under the MIT License. It is designed for building web applications and APIs.[3] It has been called the de facto standard server framework for Node.js.</p>
-                        </Card>
+                        </div>
                     </Col>
-                    <Col m={3} s={12} data-aos="flip-right">
-                        <Card className='white darken-1 skills' textClassName='black-text' actions={[<a href='https://reactjs.org'>React Homepage</a>]}>
+                    <Col m={3} s={12} data-aos="fade-up">
+                        <div className='white darken-1 skills' textClassName='black-text' actions={[<a href='https://reactjs.org'>React Homepage</a>]}>
                             <img id="react" src={react} alt="react logo" />
                             <p>In computing, React (also known as React.js or ReactJS) is a JavaScript library for building user interfaces. It is maintained by Facebook and a community of individual developers and companies.</p>
                             <p>React can be used as a base in the development of single-page or mobile applications. Complex React applications usually require the use of additional libraries for state management, routing, and interaction with an API.</p>
-                        </Card>
+                        </div>
                     </Col>
-                    <Col m={3} s={12} data-aos="flip-right">
-                        <Card className='white darken-1 skills' textClassName='black-text' actions={[<a href='https://nodejs.org'>Node Homepage</a>]}>
+                    <Col m={3} s={12} data-aos="fade-up">
+                        <div className='white darken-1 skills' textClassName='black-text' actions={[<a href='https://nodejs.org'>Node Homepage</a>]}>
                             <img id="node" src={node} alt="node.js logo" />
                             <p>Node.js is an open-source, cross-platform JavaScript run-time environment that executes JavaScript code outside the browser. Historically, JavaScript was used primarily for client-side scripting, in which scripts written in JavaScript are embedded in a webpage's HTML and run client-side by a JavaScript engine in the user's web browser. Node.js lets developers use JavaScript to write Command Line tools and for server-side scripting—running scripts server-side to produce dynamic web page content before the page is sent to the user's web browser.</p>
-                        </Card>
+                        </div>
                     </Col>
                 </Row>
                 <Row>
-                    <Col m={3} s={12} data-aos="flip-right">
-                        <Card className='white darken-1 skills' textClassName='black-text' actions={[<a href='https://mysql.com'>MySql Homepage</a>]}>
+                    <Col m={3} s={12} data-aos="fade-up">
+                        <div className='white darken-1 skills' textClassName='black-text' actions={[<a href='https://mysql.com'>MySql Homepage</a>]}>
                             <img id="mysql" src={mysql} alt="mysql logo" />
                             <p>MySQL is an open-source relational database management system (RDBMS). Its name is a combination of "My", the name of co-founder Michael Widenius's daughter, and "SQL", the abbreviation for Structured Query Language. The MySQL development project has made its source code available under the terms of the GNU General Public License, as well as under a variety of proprietary agreements.</p>
-                        </Card>
+                        </div>
                     </Col>
-                    <Col m={3} s={12} data-aos="flip-right">
-                        <Card className='white darken-1 skills' textClassName='black-text' actions={[<a href='https://docs.sequelizejs.com'>Sequelize Homepage</a>]}>
+                    <Col m={3} s={12} data-aos="fade-up">
+                        <div className='white darken-1 skills' textClassName='black-text' actions={[<a href='https://docs.sequelizejs.com'>Sequelize Homepage</a>]}>
                             <img id="sequelizeLogo" src={sequelLogo} alt="sequelize logo" />
                             <p>Sequelize is a promise-based Node.js ORM for Postgres, MySQL, SQLite and Microsoft SQL Server. It features solid transaction support, relations, read replication and more.</p>
-                        </Card>
+                        </div>
                     </Col>
-                    <Col m={3} s={12} data-aos="flip-right">
-                        <Card className='white darken-1 skills' textClassName='black-text' actions={[<a href='https://getbootstrap.com'>Bootstrap Homepage</a>]}>
+                    <Col m={3} s={12} data-aos="fade-up">
+                        <div className='white darken-1 skills' textClassName='black-text' actions={[<a href='https://getbootstrap.com'>Bootstrap Homepage</a>]}>
                             <img id="bootstrap" src={bootstrap} alt="bootstrap logo" />
                             <p>Bootstrap is a free and open-source front-end framework (library) for designing websites and web applications. It contains HTML- and CSS-based design templates for typography, forms, buttons, navigation and other interface components, as well as optional JavaScript extensions. Unlike many web frameworks, it concerns itself with front-end development only.</p>
-                        </Card>
+                        </div>
                     </Col>
-                    <Col m={3} s={12} data-aos="flip-right">
-                        <Card className='white darken-1 skills' textClassName='black-text' actions={[<a href='https://jquery.com'>JQuery Homepage</a>]}>
+                    <Col m={3} s={12} data-aos="fade-up">
+                        <div className='white darken-1 skills' textClassName='black-text' actions={[<a href='https://jquery.com'>JQuery Homepage</a>]}>
                             <img id="jq" src={jq} alt="JQuery logo" />
                             <p>jQuery is a cross-platform JavaScript library designed to simplify the client-side scripting of HTML. It is free, open-source software using the permissive MIT License. Web analysis indicates that it is the most widely deployed JavaScript library by a large margin.</p>
-                        </Card>
+                        </div>
                     </Col>
                 </Row>
             </div>
